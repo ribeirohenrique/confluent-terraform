cfb84c7ab38fb1bf6ce1b7d333af2e7d8f9af6a9
Henrique Mendes <henrique1258@gmail.com>
2023-12-06 19:36:56 -0300

    Melhorando o script e adicionando configuracao para BigQuery

---
+++ terraform-create-bigquery/main.tf
@@ -0,0 +1,211 @@
+terraform {
+  required_providers {
+    confluent = {
+      source  = "confluentinc/confluent"
+      version = "1.55.0"
+    }
+  }
+}
+
+//Define a Cloud API Key criada anteriormente
+provider "confluent" {
+  cloud_api_key    = var.confluent_cloud_api_key
+  cloud_api_secret = var.confluent_cloud_api_secret
+}
+
+
+//Define o Cluster a ser utilizado
+data "confluent_kafka_cluster" "shared_des" {
+  id = var.cluster_id
+  environment {
+    id = var.environment_id
+  }
+}
+
+
+//Cria uma Service Account para gerenciar os acessos
+resource "confluent_service_account" "app_manager" {
+  display_name = "app_${var.topic_name}_manager"
+  description  = "Service account to manage ${var.cluster_id} Kafka cluster"
+}
+
+//Atribui a Service account criada acima a Role de CloudClusterAdmin
+resource "confluent_role_binding" "app_manager_kafka_cluster_admin" {
+  principal   = "User:${confluent_service_account.app_manager.id}"
+  role_name   = "CloudClusterAdmin"
+  crn_pattern = data.confluent_kafka_cluster.shared_des.rbac_crn
+}
+
+//Cria uma API Key a nível de Cluster
+resource "confluent_api_key" "app_manager_kafka_api_key" {
+  display_name = "app_manager_kafka_api_key"
+  description  = "Kafka API Key that is owned by ${confluent_service_account.app_manager.display_name} service account"
+  owner {
+    id          = confluent_service_account.app_manager.id
+    api_version = confluent_service_account.app_manager.api_version
+    kind        = confluent_service_account.app_manager.kind
+  }
+
+  managed_resource {
+    id          = data.confluent_kafka_cluster.shared_des.id
+    api_version = data.confluent_kafka_cluster.shared_des.api_version
+    kind        = data.confluent_kafka_cluster.shared_des.kind
+
+    environment {
+      id = var.environment_id
+    }
+  }
+
+  # The goal is to ensure that confluent_role_binding.app_manager_kafka_cluster_admin is created before
+  # confluent_api_key.app_manager_kafka_api_key is used to create instances of
+  # confluent_kafka_topic, confluent_kafka_acl resources.
+
+  # 'depends_on' meta_argument is specified in confluent_api_key.app_manager_kafka_api_key to avoid having
+  # multiple copies of this definition in the configuration which would happen if we specify it in
+  # confluent_kafka_topic, confluent_kafka_acl resources instead.
+  depends_on = [
+    confluent_role_binding.app_manager_kafka_cluster_admin
+  ]
+}
+
+//Cria uma Service Account para consumir dados
+resource "confluent_service_account" "app_consumer" {
+  display_name = "app_${var.topic_name}_consumer"
+  description  = "Service account to consume from '${var.topic_name}' topic of ${var.cluster_id} Kafka cluster"
+}
+
+//Cria uma API Key a nível de Cluster
+resource "confluent_api_key" "app_consumer_kafka_api_key" {
+  display_name = "${confluent_service_account.app_consumer.display_name}_kafka_api_key"
+  description  = "Kafka API Key that is owned by ${confluent_service_account.app_consumer.display_name} service account"
+  owner {
+    id          = confluent_service_account.app_consumer.id
+    api_version = confluent_service_account.app_consumer.api_version
+    kind        = confluent_service_account.app_consumer.kind
+  }
+
+  managed_resource {
+    id          = data.confluent_kafka_cluster.shared_des.id
+    api_version = data.confluent_kafka_cluster.shared_des.api_version
+    kind        = data.confluent_kafka_cluster.shared_des.kind
+
+    environment {
+      id = var.environment_id
+    }
+  }
+}
+
+//Cria uma Service Account para consumir dados
+resource "confluent_service_account" "app_producer" {
+  display_name = "app_${var.topic_name}_producer"
+  description  = "Service account to produce to '${var.topic_name}' topic of ${var.cluster_id} Kafka cluster"
+}
+
+//Cria uma API Key a nível de Cluster
+resource "confluent_api_key" "app_producer_kafka_api_key" {
+  display_name = "${confluent_service_account.app_producer.display_name}_kafka_api_key"
+  description  = "Kafka API Key that is owned by ${confluent_service_account.app_producer.display_name} service account"
+  owner {
+    id          = confluent_service_account.app_producer.id
+    api_version = confluent_service_account.app_producer.api_version
+    kind        = confluent_service_account.app_producer.kind
+  }
+
+  managed_resource {
+    id          = data.confluent_kafka_cluster.shared_des.id
+    api_version = data.confluent_kafka_cluster.shared_des.api_version
+    kind        = data.confluent_kafka_cluster.shared_des.kind
+
+    environment {
+      id = var.environment_id
+    }
+  }
+}
+
+//Cria um tópico para gravar mensagens
+resource "confluent_kafka_topic" "connector_topic" {
+  kafka_cluster {
+    id = var.cluster_id
+  }
+
+  topic_name    = var.topic_name
+  rest_endpoint = data.confluent_kafka_cluster.shared_des.rest_endpoint
+  credentials {
+    key    = confluent_api_key.app_manager_kafka_api_key.id
+    secret = confluent_api_key.app_manager_kafka_api_key.secret
+  }
+}
+
+//Cria um tópico para gravar mensagens DLQ
+resource "confluent_kafka_topic" "connector_topic_dlq" {
+  kafka_cluster {
+    id = var.cluster_id
+  }
+
+  topic_name    = "dlq-lcc-${confluent_connector.sink.id}"
+  rest_endpoint = data.confluent_kafka_cluster.shared_des.rest_endpoint
+  credentials {
+    key    = confluent_api_key.app_manager_kafka_api_key.id
+    secret = confluent_api_key.app_manager_kafka_api_key.secret
+  }
+}
+
+//Atribui a Service account app_consumer a Role de DeveloperRead
+resource "confluent_role_binding" "app_consumer_developer_read_from_topic" {
+  principal   = "User:${confluent_service_account.app_consumer.id}"
+  role_name   = "DeveloperRead"
+  crn_pattern = "${data.confluent_kafka_cluster.shared_des.rbac_crn}/kafka=${data.confluent_kafka_cluster.shared_des.id}/topic=${confluent_kafka_topic.connector_topic.topic_name}"
+}
+
+//Atribui a Service account app_producer a Role de DeveloperWrite
+resource "confluent_role_binding" "app_consumer_developer_write_to_topic" {
+  principal   = "User:${confluent_service_account.app_producer.id}"
+  role_name   = "DeveloperWrite"
+  crn_pattern = "${data.confluent_kafka_cluster.shared_des.rbac_crn}/kafka=${data.confluent_kafka_cluster.shared_des.id}/topic=${confluent_kafka_topic.connector_topic.topic_name}"
+}
+
+//Cria o conector BigQuery utilizando a service account app_producer
+resource "confluent_connector" "sink" {
+  environment {
+    id = var.environment_id
+  }
+  kafka_cluster {
+    id = var.cluster_id
+  }
+
+  config_sensitive = {
+    "keyfile"                       = jsonencode(var.gcp_keyfile)
+  }
+
+  config_nonsensitive = {
+    "topics"                        = var.topic_name
+    "schema.context.name"           = "default"
+    "input.key.format"              = "BYTES"
+    "input.data.format"             = "AVRO"
+    "connector.class"               = "BigQuerySink"
+    "name"                          = "BigQueryTerraform"
+    "kafka.auth.mode"               = "SERVICE_ACCOUNT"
+    "kafka.service.account.id"      = confluent_service_account.app_producer.id
+    "project"                       = var.gcp_project_id
+    "datasets"                      = var.gcp_dataset_name
+    "partitioning.type"             = "INGESTION_TIME"
+    "auto.create.tables"            = "true"
+    "auto.update.schemas"           = "false"
+    "sanitize.topics"               = "true"
+    "sanitize.field.names"          = "false"
+    "time.partitioning.type"        = "DAY"
+    "allow.schema.unionization"     = "false"
+    "all.bq.fields.nullable"        = "false"
+    "convert.double.special.values" = "false"
+    "max.poll.interval.ms"          = "300000"
+    "max.poll.records"              = "500"
+    "tasks.max"                     = "1"
+    "output.data.format"            = "JSON"
+    "quickstart"                    = "ORDERS"
+    "tasks.max"                     = "1"
+  }
+
+  lifecycle {
+    prevent_destroy = false
+  }
+}
---
+++ terraform-create-bigquery/variables.tf
@@ -0,0 +1,60 @@
+variable "confluent_cloud_api_key" {
+  description = "Confluent Cloud API Key (also referred as Cloud API ID)"
+  type        = string
+  sensitive   = true
+}
+
+variable "confluent_cloud_api_secret" {
+  description = "Confluent Cloud API Secret"
+  type        = string
+  sensitive   = true
+}
+
+variable "organization_id" {
+  description = "Confluent Organization Id"
+  type        = string
+  sensitive   = false
+}
+
+variable "environment_id" {
+  description = "Confluent Environment Id"
+  type        = string
+  sensitive   = false
+}
+
+variable "cluster_id" {
+  description = "Confluent Cluster Id"
+  type        = string
+  sensitive   = false
+}
+
+variable "topic_name" {
+  description = "Confluent Topic Name"
+  type        = string
+  sensitive   = false
+}
+
+variable "rest_endpoint" {
+  description = "Confluent REST endpoint"
+  type        = string
+  sensitive   = false
+}
+
+variable "gcp_keyfile" {
+  description = "GCP JSON keyfile"
+  type = map(string)
+  sensitive = true
+  default = {}
+}
+
+variable "gcp_dataset_name" {
+  description = "GCP dataset name"
+  type = string
+  sensitive = false
+}
+
+variable "gcp_project_id" {
+  description = "GCP project id"
+  type = string
+  sensitive = false
+}
